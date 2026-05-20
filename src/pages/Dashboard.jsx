a09f40ad683cdd5f2a89086f4414fd8c9b2b8b19
sndfsecurity <sndf4315@gmail.com>
2026-05-20 11:28:36 +0530

    Initial admin panel setup

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Dashboard() {
+
+    const [enquiries, setEnquiries] = useState([]);
+
+    useEffect(() => {
+
+        fetchEnquiries();
+
+    }, []);
+
+    const fetchEnquiries = async () => {
+
+        try {
+
+            const token =
+                localStorage.getItem("token");
+
+            const response = await axios.get(
+                "http://localhost:8080/api/enquiry/my-enquiries",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            setEnquiries(response.data);
+
+        } catch (error) {
+
+            console.log(error);
+        }
+    };
+
+
+       const handleLogout = () => {
+
+    localStorage.removeItem("token");
+
+    window.location.reload();
+};
+
+    return (
+
+        <div style={{ padding: "20px" }}>
+
+            <h2>Dashboard</h2>
+
+            <button
+    onClick={handleLogout}
+    style={{
+        marginBottom: "20px",
+        padding: "10px 20px",
+        background: "red",
+        color: "white",
+        border: "none",
+        cursor: "pointer"
+    }}
+>
+    Logout
+</button>
+
+            <table
+                border="1"
+                cellPadding="10"
+                width="100%"
+            >
+
+                <thead>
+
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Service</th>
+                        <th>Department</th>
+                        <th>Status</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {
+                        enquiries.map((enquiry) => (
+
+                            <tr key={enquiry.id}>
+
+                                <td>{enquiry.id}</td>
+
+                                <td>{enquiry.fullName}</td>
+
+                                <td>{enquiry.service}</td>
+
+                                <td>{enquiry.department}</td>
+
+                                <td>{enquiry.status}</td>
+
+                            </tr>
+                        ))
+                    }
+
+                </tbody>
+
+            </table>
+
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+
+        try {
+
+            const response = await axios.post(
+                "http://localhost:8080/api/auth/login",
+                {
+                    username,
+                    password
+                }
+            );
+
+            console.log(response.data);
+
+            localStorage.setItem(
+                "token",
+                response.data.token
+            );
+
+            window.location.reload();
+
+            alert("Login Successful");
+
+        } catch (error) {
+
+            alert("Invalid Credentials");
+        }
+    };
+
+
+ 
+
+    return (
+
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                background: "#f1f5f9"
+            }}
+        >
+
+            <div
+                style={{
+                    width: "350px",
+                    background: "white",
+                    padding: "30px",
+                    borderRadius: "10px",
+                    boxShadow: "0 0 10px rgba(0,0,0,0.1)"
+                }}
+            >
+
+                <h2
+                    style={{
+                        textAlign: "center",
+                        marginBottom: "20px"
+                    }}
+                >
+                    Admin Login
+                </h2>
+
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) =>
+                        setUsername(e.target.value)
+                    }
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        marginBottom: "15px"
+                    }}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) =>
+                        setPassword(e.target.value)
+                    }
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        marginBottom: "20px"
+                    }}
+                />
+
+                <button
+                    onClick={handleLogin}
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        background: "#1677ff",
+                        color: "white",
+                        border: "none",
+                        cursor: "pointer"
+                    }}
+                >
+                    Login
+                </button>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Login;
